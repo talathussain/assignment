8793efbb96c34c73b51b4f1152bafc0d5274e852
Talat <30823797+talathussain@users.noreply.github.com>
2017-11-16 03:37:47 -0500

    Update Parameter.py

--- assignment_2/Parameter.py
+++ assignment_2/Parameter.py
@@ -1,10 +1,9 @@
-class ClassAbc:
-	name="This Is a Class Variable" 
-	def __init__(self,  name):
-		self.n = name 
-print ClassAbc.name 
-ClassAbc.name= "Class Varible Changed Here"
-print ClassAbc.name
-obj = ClassAbc("This Is an Instance Variable")    
-print obj.name      
-
+class ClassA:
+	num = 10 
+	def __init__(self,  num):
+		self.n = 12 
+print ("Class Variable",ClassA.num) 
+ClassA.num= 15
+print ("Class Variable Changed Here",ClassA.num)
+obj = ClassA(5)    
+print ("This Is an Instance Variable",obj.n)      
