f179c29a27754e50c400f9710a9f538b0d648e07
Talat <30823797+talathussain@users.noreply.github.com>
2017-11-16 03:36:21 -0500

    Update Formula.py

--- assignment_2/Formula.py
+++ assignment_2/Formula.py
@@ -1,18 +1,8 @@
-print("Find Q = Square Root of[(2*C*D)/H] Where C= 50, H=30")
-while True:
-	try:	
-		import math
-		c=50
-		h=30
-		value = []
-		items=[x for x in raw_input("Enter A Value of D__").split(',')]
-		for d in items:
-		    value.append(str(int(math.sqrt(2*c*float(d)/h))))
-
-		print ("Value of Q ="),','.join(value)
-		break
-		
-	except:
-		print"Sorry! Please Enter an Integer"
-		continue
-#print(math.__doc__)
+import math
+C=50
+H=30
+value = []
+items=[x for x in input("Enter A Value of D = ").split(',')]
+for d in items:
+	value.append(str(int(math.sqrt(2*C*float(d)/H))))
+print ("Value of Q =",','.join(value))
