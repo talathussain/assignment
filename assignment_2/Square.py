feea97afc8798627725131d7082f44dc185a1228
Talat <30823797+talathussain@users.noreply.github.com>
2017-11-21 15:13:29 +0500

    Delete Square.py

--- assignment_2/Square.py
+++
@@ -1,9 +0,0 @@
-while True:
-	try:
-		val = int(input("Enter Nubmer of square = "))	
-		print("Square of", val,  "is = ", val*val)
-		break
-	except:
-        	print ("Please enter integer Number")
-	
-	
