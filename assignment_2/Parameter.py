5f94595c32a6c03da3ea0f858e3656ed75a28993
Talat <30823797+talathussain@users.noreply.github.com>
2017-11-21 15:12:58 +0500

    Delete Parameter.py

--- assignment_2/Parameter.py
+++
@@ -1,9 +0,0 @@
-class ClassA:
-	num = 10 
-	def __init__(self,  num):
-		self.n = 12 
-print ("Class Variable",ClassA.num) 
-ClassA.num= 15
-print ("Class Variable Changed Here",ClassA.num)
-obj = ClassA(5)    
-print ("This Is an Instance Variable",obj.n)      
